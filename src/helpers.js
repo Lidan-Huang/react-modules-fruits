0ddc7fc6a89bfb6a3d0b016f97ba85b4db995f56
Lidan-Huang <huangld0314@gmail.com>
2022-10-03 16:31:26 -0700

    added some documentations

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,11 @@
-
+/**Choose a random item from given array of items. */
 function choice(items) {
   return items[Math.floor(Math.random() * items.length)];
 }
 
+/**Remove the first matching item from items, if it exists, and
+ * returns it. Otherwise returns undefined.
+ */
 function remove(items, item) {
   const itemIdx = items.indexOf(item);
   return (itemIdx === -1)
